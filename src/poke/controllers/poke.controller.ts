a0ed4d1a036f546acc1f3da868cd812432672639
lindseydortch <codewithlinds@gmail.com>
2021-10-03 16:32:37 -0500

    able to display all pokemon in the backend

--- src/poke/controllers/poke.controller.ts
+++ src/poke/controllers/poke.controller.ts
@@ -1,13 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { PokeService } from '../providers/poke.service';
-import { Poke } from '../interfaces/poke.interface';
+// import { Poke } from '../interfaces/poke.interface';
 
 @Controller('poke')
 export class PokeController {
   constructor(private pokeService: PokeService) {}
 
+  // Find All Pokemon
   @Get()
-  async findAll(): Promise<Poke[]> {
-    return this.pokeService.findAll();
+  findAll() {
+    const res = this.pokeService.findAll();
+    return res;
+  }
+
+  // Find by ID
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    return `This returns a pokemon by id. The id of this pokemon is ${id}`;
   }
 }
